acc1872479368b5f794921369486d833e1a9e7c8
Ludgero Bezerra <ludgerobezerrasuporte@gmail.com>
2022-03-24 21:00:35 -0300

    Criando classe livro

---
+++ src/main/java/br/edu/catolica/ppi/nossaBiblioteca/model/Livro.java
@@ -0,0 +1,27 @@
+package br.edu.catolica.ppi.nossaBiblioteca.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String nome;
+    private int esbn;
+
+}
